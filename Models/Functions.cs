8e8f9a30a7c7812d3deaf1bed139435a6cb5b8c7
Omkar Raut <omkar.raut.3551@gmail.com>
2024-08-12 20:11:20 +0530

    first commit

---
+++ Models/Functions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Functions
+    {
+        private SqlConnection Con;
+        private SqlCommand cmd;
+        private DataTable dt;
+        private SqlDataAdapter sda;
+        private string ConnString;
+
+        public Functions() 
+        {
+            ConnString = "Data Source=JBLOAD\\SQLEXPRESS;Initial Catalog=OnlineGroceryManagement;Integrated Security=True;Pooling=False";
+            Con= new SqlConnection(ConnString);
+            cmd = new SqlCommand();
+            cmd.Connection = Con;
+
+        }
+        public DataTable GetData(string Query)
+        {
+            dt = new DataTable();
+            sda = new SqlDataAdapter(Query,ConnString);
+            sda.Fill(dt);
+            return dt;
+        }
+        public int SetData(string Query)
+        {
+            int Cnt = 0;
+            if(Con.State==ConnectionState.Closed)
+            {
+                Con.Open();
+
+            }
+            cmd.CommandText= Query;
+            Cnt =cmd.ExecuteNonQuery();
+            return Cnt; 
+        }
+    }
+}   
